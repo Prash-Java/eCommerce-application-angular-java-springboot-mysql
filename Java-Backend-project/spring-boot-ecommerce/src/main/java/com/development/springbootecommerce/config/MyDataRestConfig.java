0508baf8ee31d5bc1575701338340c0e58c28528
prash <prash.sastra1@gmail.com>
2023-05-30 22:47:40 +0530

    adding config

---
+++ Java-Backend-project/spring-boot-ecommerce/src/main/java/com/development/springbootecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.development.springbootecommerce.config;
+
+import com.development.springbootecommerce.entity.Product;
+import com.development.springbootecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.HttpMethods;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST,HttpMethod.DELETE,HttpMethod.PUT};
+        //Disable for Product Entity: POST, DELETE, PUT Http Methods
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //Disable for Product Category Entity: POST, DELETE, PUT http methods
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
