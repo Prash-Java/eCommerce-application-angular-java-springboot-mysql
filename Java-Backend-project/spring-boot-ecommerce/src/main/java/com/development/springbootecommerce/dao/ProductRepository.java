387143fd3d34d7fd3d76c4eb3dc4d1bf67c6112e
prash <prash.sastra1@gmail.com>
2023-06-01 21:25:02 +0530

    integrating Angular components and services commit

--- Java-Backend-project/spring-boot-ecommerce/src/main/java/com/development/springbootecommerce/dao/ProductRepository.java
+++ Java-Backend-project/spring-boot-ecommerce/src/main/java/com/development/springbootecommerce/dao/ProductRepository.java
@@ -2,8 +2,9 @@ package com.development.springbootecommerce.dao;
 
 import com.development.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin("http://localhost:4200") // origin = protocol + hostname + portNumber
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
