4fcb44201353bb5e9d4a0ee07a5fe699d828c268
prash <prash.sastra1@gmail.com>
2023-06-04 20:26:52 +0530

    Springboot rest endpoint for Products by category and Angular routes, router

--- Angular-Frontend-Project/angular-frontend/src/app/app.module.ts
+++ Angular-Frontend-Project/angular-frontend/src/app/app.module.ts
@@ -5,6 +5,20 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes:Routes = [
+  // Angular by default adds '/' in path like '/category' and hence we do not need to add '/' explicitly,
+  // Angular advices to add routes in specific to generic manner, coming from top to down approach here below, 1st, 2nd, 3rd paths being specific
+  // If first three specific paths do not matches, then we give 4th one with redirectTo but only this will start path with '/' explicitly
+  // If none of 1st four matches, then generic wildcard is used in 5th case below,
+  // NOTE: Being single page application, only modified part of page gets updated, and not the entire page
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component:ProductListComponent},
+  { path: 'products', component:ProductListComponent},
+  { path: '', redirectTo: '/products', pathMatch: 'full'},
+  { path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +26,8 @@ import { ProductService } from './services/product.service';
     ProductListComponent
   ],
   imports: [
+    // We just import the above created routes here in 1st line using Router Module
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
--- Angular-Frontend-Project/angular-frontend/src/app/components/product-list/product-list.component.ts
+++ Angular-Frontend-Project/angular-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,28 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
-  // here we would integrate this component with product service using dependency injection,
-  constructor(private productService: ProductService) { }
+  currentCategoryId: number = 1;
+  //  here we would integrate this component with product service using dependency injection,
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
-}
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+  }
 
-listProducts(){
-  this.productService.getProductList().subscribe(
-    data => {
-      this.products = data;
+  listProducts() {
+    // Check if categoryId is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    // If CategoryId is available, convert it into number using "+" operator to read that id as number, else assign it value of 1 as default
+    if(hasCategoryId){
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1;
     }
-  )
-}
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 }
