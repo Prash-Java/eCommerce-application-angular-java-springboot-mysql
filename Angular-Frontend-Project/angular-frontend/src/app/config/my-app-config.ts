dbea0915833f070d5408c188e969a396fcb8baf4
prash <prash.sastra1@gmail.com>
2023-06-21 22:00:47 +0530

    Started on Security config with Okta

---
+++ Angular-Frontend-Project/angular-frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oaa2eri5oeLGWrIk5d7',
+        issuer: 'https://dev-59247342.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
