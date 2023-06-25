65f0ac068b41233a213b13400f4c5b24f2a7933c
prash <prash.sastra1@gmail.com>
2023-06-25 21:25:10 +0530

    Updates on my-app-config.ts

--- Angular-Frontend-Project/angular-frontend/src/app/config/my-app-config.ts
+++ Angular-Frontend-Project/angular-frontend/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc: {
-        clientId: '0oaa3qo4uxxtSa3gW5d7',
-        issuer: 'https://dev-59247342.okta.com/oauth2/default',
+        clientId: '0oaa4efhzqpPvURtl5d7',
+        issuer: 'http://dev-59247342.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
