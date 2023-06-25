727e0981cd4c416ebcf52d35f0c4f1856cd304f6
prash <prash.sastra1@gmail.com>
2023-06-25 21:39:33 +0530

    Done with Okta Security Config for Login status component in .ts file

--- Angular-Frontend-Project/angular-frontend/src/app/config/my-app-config.ts
+++ Angular-Frontend-Project/angular-frontend/src/app/config/my-app-config.ts
@@ -1,8 +1,10 @@
 export default {
     oidc: {
         clientId: '0oaa4efhzqpPvURtl5d7',
-        issuer: 'http://dev-59247342.okta.com/oauth2/default',
+        issuer: 'https://dev-59247342.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
+
+// In App, the username and password would be as "username":"prashantkumar_choud@hcl.com" and "password":"Angular_Java_01"
