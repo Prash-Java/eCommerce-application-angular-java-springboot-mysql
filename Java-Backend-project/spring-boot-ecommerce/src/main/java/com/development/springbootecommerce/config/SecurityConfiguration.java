51411bcd9f78ffe87778e35f0cb4f887cd879dc5
prash <prash.sastra1@gmail.com>
2023-07-04 21:56:20 +0530

    Springboot security config for the endpoint /api/orders/** at Java backend

--- Java-Backend-project/spring-boot-ecommerce/src/main/java/com/development/springbootecommerce/config/SecurityConfiguration.java
+++ Java-Backend-project/spring-boot-ecommerce/src/main/java/com/development/springbootecommerce/config/SecurityConfiguration.java
@@ -1,2 +1,44 @@
-package com.development.springbootecommerce.config;public class SecurityConfiguration {
+package com.development.springbootecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //protect endpoint /api/orders
+        http.authorizeRequests(configurer ->
+                configurer
+                                .requestMatchers("/api/orders/**")
+                                .authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+
+
+
+        //add CORS filters
+        http.cors(Customizer.withDefaults());
+
+        //add content negotiation strategy to show 401 Unauthorised
+        http.setSharedObject(ContentNegotiationStrategy.class, new
+                HeaderContentNegotiationStrategy());
+
+        //force a non-empty response body for 401's to make the response more friendly
+
+        Okta.configureResourceServer401ResponseBody(http);
+
+        // disable CSRF since we are not using Cookies for session tracking
+        http.csrf((csrf) -> csrf.disable());
+
+        return http.build();
+    }
 }
