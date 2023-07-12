390ea0f6b6b15c4f5a3b0a5791015c3dfa90c04c
prash <prash.sastra1@gmail.com>
2023-07-12 22:43:31 +0530

    Starting with payment integration using Stripe under Payment Processors

--- Java-Backend-project/spring-boot-ecommerce/src/main/java/com/development/springbootecommerce/dto/PaymentInfo.java
+++ Java-Backend-project/spring-boot-ecommerce/src/main/java/com/development/springbootecommerce/dto/PaymentInfo.java
@@ -1,2 +1,9 @@
-package com.development.springbootecommerce.dto;public class PaymentInfo {
+package com.development.springbootecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+    private int amount;
+    private String currency;
 }
